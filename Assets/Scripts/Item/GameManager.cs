10ff028ac9254d2657f7dd43d820f9866c009b9a
KimM1nSang <minsang1008@naver.com>
2021-10-21 18:16:14 +0900

    게임매니저 수정

--- Assets/Scripts/Item/GameManager.cs
+++ Assets/Scripts/Item/GameManager.cs
@@ -4,17 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-<<<<<<< HEAD
     public Transform playerTR;
-    #region 싱글톤
     public static GameManager instance;
-
-=======
-    #region 싱글톤
-    private GameManager instance;
-    public GameManager Instance;
->>>>>>> e3b12ec47bf30c2db29a94aff25abd25c98439b8
-
     private void Awake()
     {
         if (instance != null)
@@ -26,13 +17,11 @@ public class GameManager : MonoBehaviour
             instance = this;
         }
     }
-    #endregion
 
     private void OnDestroy()
     {
         instance = null;
     }
-<<<<<<< HEAD
 
     public static Transform GetPlayer()
     {
@@ -42,7 +31,3 @@ public class GameManager : MonoBehaviour
 
 
 }
-=======
-    #endregion
-}
->>>>>>> e3b12ec47bf30c2db29a94aff25abd25c98439b8
